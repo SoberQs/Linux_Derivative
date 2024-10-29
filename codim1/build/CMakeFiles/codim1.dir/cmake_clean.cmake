6360199cf884c9f7d19d06e2c271f457e60767bf
Yidong <qiuyidong0107@163.com>
2024-10-29 17:10:04 +0000

    Updated the directory structure and added CMakeLists

---
+++ codim1/build/CMakeFiles/codim1.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/codim1.dir/codim1.c.o"
+  "CMakeFiles/codim1.dir/codim1.c.o.d"
+  "codim1"
+  "codim1.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/codim1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ codim2/build/CMakeFiles/codim2.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/codim2.dir/codim2.cc.o"
+  "CMakeFiles/codim2.dir/codim2.cc.o.d"
+  "codim2"
+  "codim2.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/codim2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
